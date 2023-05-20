19acbece7d13e8cad7125006f6f23a4d82f37c32
Jose <jsut914@gmail.com>
2023-05-20 17:38:10 -0600

    Mejorando la estructura login js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-function registro(){
+//codigo registro
+const login = document.querySelector('[data-from-btnRegistro]'); 
+
+login.addEventListener('click',function registro(){
     location.href = "login.html"; 
-}
+}); 
