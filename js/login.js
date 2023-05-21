e0df6a3120368465fe3beb07853a3864f3afaf07
Jose <jsut914@gmail.com>
2023-05-21 15:43:16 -0600

    Modificando la calidad de la imagen

--- js/login.js
+++ js/login.js
@@ -1,35 +1,39 @@
-class datosUsuario{
-    #nombre; 
-    #password; 
+( ()=>{
 
-    constructor(){
-        this.#nombre = ""; 
-        this.#password = ""; 
-    }
-}
-const entrar = document.querySelector('[data-from-btnEntrar]');
-
-const cliente0 = new datosUsuario(); 
-const dato1 = cliente0.nombre = "nohemi914@gmail.com"; 
-const dato2 = cliente0.password = "Bodoque05/11";  
-
-const registroDatos = (evento)=>{
-    evento.preventDefault(); 
-    const usuario = document.querySelector('[data-nombre]'); 
-    const password = document.querySelector('[data-password]'); 
+    class datosUsuario{
+        #nombre; 
+        #password; 
     
-    datosCliente(usuario.value, password.value);  
-};
-
-entrar.addEventListener('click', registroDatos); 
-
-function datosCliente(nombre, password){
-    const usuario = nombre; 
-    const contrasenhia = password; 
-
-    if(usuario == dato1 && contrasenhia==dato2){  
-        location.href = "productos.html";        
-    }else{
-        console.log("ERROR"); 
+        constructor(){
+            this.#nombre = ""; 
+            this.#password = ""; 
+        }
     }
-}; 
+    const entrar = document.querySelector('[data-from-btnEntrar]');
+    
+    const cliente0 = new datosUsuario(); 
+    const dato1 = cliente0.nombre = "algeek4@gmail.com"; 
+    const dato2 = cliente0.password = "frontEnd2023";  
+    
+    const registroDatos = (evento)=>{
+        evento.preventDefault(); 
+        const usuario = document.querySelector('[data-nombre]'); 
+        const password = document.querySelector('[data-password]'); 
+        
+        datosCliente(usuario.value, password.value);  
+    };
+    
+    entrar.addEventListener('click', registroDatos); 
+    
+    function datosCliente(nombre, password){
+        const usuario = nombre; 
+        const contrasenhia = password; 
+    
+        if(usuario == dato1 && contrasenhia==dato2){  
+            location.href = "productos.html";        
+        }else{
+            console.log("ERROR"); 
+        }
+    }; 
+
+}) (); 
